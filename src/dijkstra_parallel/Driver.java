738c0a0d470e8da356c4b2bb8dac64918d893815
Jonathan Eanes <jdeanes0@frostburg.edu>
2025-03-10 14:02:20 -0400

    add parallel folder with stub file

---
+++ src/dijkstra_parallel/Driver.java
@@ -0,0 +1,10 @@
+package dijkstra_parallel;
+
+/**
+ * This is the driver class for the larger Dijkstra's Algorithm.
+ *
+ * It will spawn the required threads for the algorithm to truly make it a parallel algorithm
+ */
+
+public class Driver {
+}
