52a0169c0bced9478ec432f0ae82dbf96d2370ad
AlexBlue89 <axpdignan@gmail.com>
2025-04-01 12:43:19 -0400

    commit

--- src/dijkstra_sequential/DijkstraAlgorithm.java
+++ src/dijkstra_sequential/DijkstraAlgorithm.java
@@ -28,7 +28,7 @@ public class DijkstraAlgorithm {
             long endTime = System.nanoTime(); // End timing
             long duration = endTime - startTime; // Calculate duration
             System.out.println("Time taken for jump " + (count + 1) + ": " + duration + " nanoseconds"); // Print time taken
-
+            System.out.println("Node - " + count); // Print time taken
             visited[u] = true;
 
             // Update the distance of the adjacent vertices of the picked vertex
